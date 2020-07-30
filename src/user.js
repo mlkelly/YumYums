166ba2c5a2ac775b8a9d8a22e5229897bdaebe8a
mlkelly <maurak87@gmail.com>
2020-07-30 02:12:55 -0400

    broken refactoring

--- src/user.js
+++ src/user.js
@@ -42,7 +42,7 @@ login.addEventListener("click", ()=> loginToggleHelper())
 // login button toggle inside header
 loginHeaderToggle.addEventListener("click", ()=> loginToggleHelper())
 
-// helper method for login toggles
+// login toggle helper/callback
 function loginToggleHelper(){
     console.log("login header toggle clicked")
     loginBoolean = !loginBoolean;
@@ -80,47 +80,7 @@ loginForm.addEventListener("submit", ()=>{
             console.log(localStorage)
             console.log("login submited")
 
-            // hide login toggle and reset form 
-            // loginBoolean = false
-            loginBoolean = !loginBoolean;
-            if (loginBoolean) {
-                loginForm.style.display="block"
-                loginHeaderToggle.innerText="Hide Login"
-                // headerDiv.removeChild(loginError)
-            } else {
-                loginForm.style.display="none"
-                loginHeaderToggle.innerText="Login"
-            }
-
-            if(headerDiv.lastChild === loginError){headerDiv.removeChild(loginError)} // if no error occurs, nothing to delete
-            headerDiv.removeChild(loginHeaderToggle)
-            mainBodyDiv.innerHTML=""
-
-            profileInfo.innerText= `username: ${userInfo.user.username}`
-            if (userInfo.user.bio){
-                profileInfo2.innerText = "bio: " + `${userInfo.user.bio}`
-            } else {
-                profileInfo2.innerText = "bio: *no current bio*"
-            }
-            current_user = userInfo.user
-            editProfileToggle.innerText = "Edit Profile"
-
-            editUsernameInput.value = current_user.username
-            editUsernameInput.placeholder = "Username..."
-            editBioInput.value = current_user.bio
-            editBioInput.placeholder = "Bio..."
-            editProfileSubmit.type="submit"
-            editProfileForm.style.display="none"
-            editProfileForm.append(editUsernameInput, editBioInput, editProfileSubmit)
-            deleteUserBtn.type="button"
-            deleteUserBtn.innerText ="Delete Account"
-
-            userRecipes.innerText ="My Recipes"
-            logoutBtn.innerText="Log Out"
-    
-            headerDiv.append(logoutBtn)
-            underline.append(userRecipes)
-            mainBodyDiv.append(profileInfo, profileInfo2, editProfileForm, editProfileToggle, deleteUserBtn, underline)
+            loggedinPage(userInfo)
         } else { 
             console.log("login submit failed")
             loginError.innerText = "Incorrect username or password"
@@ -129,29 +89,84 @@ loginForm.addEventListener("submit", ()=>{
     })
 })
 
+function loggedinPage(userInfo){
+    // hide login toggle and reset form   
+    // loginBoolean = false
+    loginBoolean = !loginBoolean;
+    if (loginBoolean) {
+        loginForm.style.display="block"
+        loginHeaderToggle.innerText="Hide Login"
+        // headerDiv.removeChild(loginError)
+    } else {
+        loginForm.style.display="none"
+        loginHeaderToggle.innerText="Login"
+    }
+
+    // deletes loginError from DOM once login validated
+    if(headerDiv.lastChild === loginError){headerDiv.removeChild(loginError)} // if no error occurs, nothing to delete
+    headerDiv.removeChild(loginHeaderToggle)
+
+    // removes login form, fresh canvas
+    mainBodyDiv.innerHTML=""
+    current_user = userInfo.user //passing userInfo for reference elsewhere
+    displayProfile(current_user) //still works even if signed out
+
+    // appending buttons to DOM
+    userRecipes.innerText ="My Recipes"
+    underline.append(userRecipes)
+    mainBodyDiv.prepend(underline) //only way to have 'My Recipes' not move when profileBtn clicked
+
+    profileBtn.innerText="Profile"
+    logoutBtn.innerText="Log Out"
+    headerDiv.append(profileBtn, logoutBtn)
+
+}
+
+// helper method to display only profile info 
+function displayProfile(current_user){
+    // filling/appending profile info to DOM
+    profileAvatar.src = current_user.img
+    profileInfo.innerText= `username: ${current_user.username}`
+    if (current_user.bio){
+        profileInfo2.innerText = "bio: " + `${current_user.bio}`
+    } else {
+        profileInfo2.innerText = "bio: *no current bio*"
+    }
+    // current_user = userInfo.user //passing userInfo for reference elsewhere
+    editProfileToggle.innerText = "Edit Profile" 
+    editUsernameInput.value = current_user.username
+    editUsernameInput.placeholder = "Username..."
+    editBioInput.value = current_user.bio
+    editBioInput.placeholder = "Bio..."
+    editAvatarInput.value = current_user.img
+    editAvatarInput.placeholder = "Image URL"
+    editProfileSubmit.type="submit"
+    editProfileForm.style.display="none"
+    editProfileForm.append(editUsernameInput, editBioInput, editAvatarInput, editProfileSubmit)
+    deleteUserBtn.type="button"
+    deleteUserBtn.innerText ="Delete Account"
+    
+    mainBodyDiv.append(profileAvatar, profileInfo, profileInfo2, editProfileForm, editProfileToggle, deleteUserBtn)
+}
+
+// logoutBtn functionality
 logoutBtn.addEventListener("click", ()=>{
     localStorage.clear()
     mainBodyDiv.innerHTML=""
     // debugger
     // loginForm.style.display="block"
     loginForm.reset()
+    headerDiv.removeChild(logoutBtn)
+    mainBodyDiv.append(signUpForm)
+    headerDiv.removeChild(profileBtn) //removes bc profileBtn works even when logged out bc of current_user variable
     headerDiv.append(loginHeaderToggle)
 })
 
-// loging into page takes to you user profile
-// loginBtn.addEventListener("click", ()=>{
-//     event.preventDefault()
-//     console.log("login submited")
-//     console.log(localStorage)
-
-//     mainBodyDiv.innerHTML=""
-
-//     profileInfo.innerText= "username: "
-//     userRecipes.innerText = "My Recipes"
-    
-//     underline.append(userRecipes)
-//     mainBodyDiv.append(logoutBtn, profileInfo,underline)
-// })
+// profileBtn functionality
+profileBtn.addEventListener("click", ()=>{
+    console.log("clicked profileBtn, now on profile view")
+    displayProfile(current_user)
+})
 
 // show My Recipes
 userRecipes.addEventListener("click", ()=>{
@@ -168,11 +183,18 @@ userRecipes.addEventListener("click", ()=>{
     createRecipeBtn.innerText="Create Recipe"
     // create default userInput input to loged in user
 
+    userInput.value= current_user.id
+    userInput.id="user-id"
     userInput.type="hidden"
     recipeForm.append(titleInput, abtInput, imgInput, userInput, createRecipeBtn)
     recipeForm.style.display="none"
 
     mainBodyDiv.append(pageTitle, recipeForm, br, recipeBtnToggle)
+    fetchRecipes()
+})
+
+// grabs current_user's recipes and displays them
+function fetchRecipes(){
     // debugger
     fetch(`http://localhost:3000/api/v1/users/${current_user.id}`, {
         method: "GET",
@@ -185,15 +207,16 @@ userRecipes.addEventListener("click", ()=>{
     //     debugger //lets me look into user which is essentially http://localhost:3000/api/v1/users/${current_user.id} since browser throws error bc no headers are passed (aka not logged in)
     // })
     .then(user => {
-        if(user.recipes.length > 0){
-            user.recipes.forEach(recipe => addRecipe(recipe))
-        } else {
-            noRecipeError.innerText="You have no recipes. Click on the 'Create Recipe' button to get started"
-            mainBodyDiv.append(noRecipeError)
-            console.log("this user has no recipes")
-        }
+        debugger
+        // if(user.recipes){
+        //     user.recipes.forEach(recipe => addRecipe(recipe))
+        // } else {
+        //     noRecipeError.innerText="You have no recipes. Click on the 'Create Recipe' button to get started"
+        //     mainBodyDiv.append(noRecipeError)
+        //     console.log("this user has no recipes")
+        // }
     })
-})
+}
 
 // refactored in userRecipes event listener
 // function showRecipes(recipes){
@@ -213,6 +236,7 @@ recipeBtnToggle.addEventListener("click", ()=>{
     }
 })
 
+// displays single recipe to DOM
 function addRecipe(recipe){
     recipeCard.className="card"
     recipeCardContainer.className="container"
@@ -255,19 +279,30 @@ editProfileToggle.addEventListener("click", ()=> editProfileToggleHelper())
 
 // edit profile toggle helper function
 function editProfileToggleHelper(){
+    // debugger
     console.log("Edit profile toggle clicked")
     editProfileBoolean = !editProfileBoolean;
     if (editProfileBoolean) {
         editProfileForm.style.display="block"
         // remove the p tags with the username and bio from DOM
+        // profileInfo=""
+        // profileInfo2=""
         mainBodyDiv.removeChild(profileInfo)
         mainBodyDiv.removeChild(profileInfo2)
+        mainBodyDiv.removeChild(profileAvatar)
         editProfileToggle.innerText="Cancel Edits"
     } else {
         editProfileForm.style.display="none"
         // add the p tags with the username and bio from DOM back
-        mainBodyDiv.insertBefore(profileInfo, mainBodyDiv.children[0])
-        mainBodyDiv.insertBefore(profileInfo2, mainBodyDiv.children[1])
+        // profileInfo.innerText = `username: ${userInfo.username}`
+        // if (userInfo.bio){
+        //     profileInfo2.innerText = "bio: " + `${userInfo.bio}`
+        // } else {
+        //     profileInfo2.innerText = "bio: *no current bio*"
+        // }
+        mainBodyDiv.insertBefore(profileAvatar, mainBodyDiv.children[1])
+        mainBodyDiv.insertBefore(profileInfo, mainBodyDiv.children[2])
+        mainBodyDiv.insertBefore(profileInfo2, mainBodyDiv.children[3])
         editProfileToggle.innerText="Edit Profile"
     }
 }
@@ -279,26 +314,30 @@ editProfileForm.addEventListener("submit", ()=>{
     let configObj = {
         method: "PATCH",
         headers: {
-            "Content-Type":"application/json", //throws [object Object] error?
+            // "Content-Type":"application/json", //throws [object Object] error?
             Authorization: `Bearer ${localStorage.token}` //sending auth token 
         },
         body: JSON.stringify({
             username: event.target[0].value,
-            bio: event.target[1].value
+            bio: event.target[1].value,
+            img: event.target[2].value
         })
     }
-    // debugger
+    debugger
     fetch(`http://localhost:3000/api/v1/users/${current_user.id}`, configObj) 
     .then(res => res.json())
     .then(userInfo => {
         console.log(userInfo)
         console.log("showing updated info")
-        profileInfo.innerText = `username: ${userInfo.username}`
-        if (userInfo.bio){
-            profileInfo2.innerText = "bio: " + `${userInfo.bio}`
+        // mainBodyDiv.innerHTML=""
+        profileAvatar.src = event.target[2].value
+        profileInfo.innerText = `username: ${event.target[1].value}`
+        if (event.target[1].value){
+            profileInfo2.innerText = "bio: " + `${event.target[1].value}`
         } else {
             profileInfo2.innerText = "bio: *no current bio*"
         }
+        current_user = userInfo
         editProfileToggleHelper()
         // editProfileBoolean = !editProfileBoolean
         // if (editProfileBoolean){
@@ -328,7 +367,33 @@ deleteUserBtn.addEventListener("click", ()=>{
     fetch(`http://localhost:3000/api/v1/users/${current_user.id}`, configObj)
     .then(res => {
         // console.log(res)
+
+        // resets mainBody to intro page (login/signup)
         mainBodyDiv.innerHTML=""
         mainBodyDiv.append(loginForm, loginHeaderToggle)
     })
+})
+
+
+// recipeForm manipulations 
+recipeForm.addEventListener("submit",()=>{
+    event.preventDefault()
+    console.log("inside recipe form")
+
+    // post request to http://localhost:3000/api/v1/recipes
+    let configObj ={ 
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${localStorage.token}` //sending auth token
+        },
+        body: JSON.stringify({
+            title: recipeForm[0].value,
+            abt: recipeForm[1].value,
+            img: recipeForm[2].value,
+            user_id: current_user.id
+        })
+    }
+    debugger
+    fetch("http://localhost:3000/api/v1/recipes", configObj)
+    .then(console.log)
 })
